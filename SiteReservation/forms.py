cc527367ec384daeeaba8a1f3ab48650e424f457
David <youchen.du@gmail.com>
2017-09-15 19:39:26 +0800

    [update] travis config.

--- SiteReservation/forms.py
+++ SiteReservation/forms.py
@@ -3,7 +3,7 @@
 # Author: David
 # Email: youchen.du@gmail.com
 # Created: 2017-09-14 14:39
-# Last modified: 2017-09-15 14:51
+# Last modified: 2017-09-15 19:36
 # Filename: forms.py
 # Description:
 from django import forms
@@ -13,5 +13,5 @@ class DateForm(forms.Form):
     date = forms.DateField(
         label='日期',
         widget=forms.TextInput(attrs={
-            'readonly':'true',
+            'readonly': 'true',
             'class': 'form-control'}))
