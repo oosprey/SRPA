5f526b4ae1730a29fd5be65c0e385a051303f9ce
eyeeco <1911362549@qq.com>
2017-09-15 04:10:38 -0700

    9.15 mid

--- static/js/auth.js
+++ static/js/auth.js
@@ -1,6 +1,8 @@
 function project_callback(data){
+    // pass something
     var project_item = $('#project_item');
     project_item.fadeOut(0);
     $('#project_item').html(data);
     project_item.fadeIn(500);
-}
+    // tag changed
+    }
