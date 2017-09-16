bd10af0c02288806ad783266ff6c8db5680fcb1f
David <youchen.du@gmail.com>
2017-09-16 17:09:06 +0800

    [fix] flake8.

--- authentication/context_processors.py
+++ authentication/context_processors.py
@@ -3,11 +3,12 @@
 # Author: David
 # Email: youchen.du@gmail.com
 # Created: 2017-09-16 10:55
-# Last modified: 2017-09-16 10:58
+# Last modified: 2017-09-16 17:09
 # Filename: context_processors.py
 # Description:
 from authentication import USER_IDENTITY_STUDENT, USER_IDENTITY_TEACHER
 from authentication import USER_IDENTITY_ADMIN
+
 
 def expose_consts(request):
     consts = {
