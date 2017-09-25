488589101370a1d68f4880b8f14a09b87de2b01d
David <youchen.du@gmail.com>
2017-09-25 17:26:22 +0800

    [update] Invited registration.

--- authentication/forms.py
+++ authentication/forms.py
@@ -3,7 +3,7 @@
 # Author: David
 # Email: youchen.du@gmail.com
 # Created: 2017-09-07 09:09
-# Last modified: 2017-09-14 09:50
+# Last modified: 2017-09-25 17:26
 # Filename: forms.py
 # Description:
 from django import forms
@@ -11,8 +11,9 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 
-from .models import UserInfo, StudentInfo
 from const.models import CaptchaField
+from .models import UserInfo, StudentInfo
+from .allow_ids import ALLOW_IDS
 
 
 class LoginForm(AuthenticationForm):
@@ -72,3 +73,9 @@ class StudentRegisterForm(RegisterForm):
         model = StudentInfo
         fields = ['username', 'password', 'confirm_password',
                   'name', 'phone', 'student_id', 'institute', 'captcha']
+
+    def clean_student_id(self, *args, **kwargs):
+        student_id = self.cleaned_data['student_id']
+        if student_id not in ALLOW_IDS:
+            raise forms.ValidationError('学号不在受邀注册名单内')
+        return student_id
