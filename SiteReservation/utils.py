0b7dbc6e41c9c332fedc060c6f6a9d6301d4d670
David <youchen.du@gmail.com>
2017-09-25 09:32:17 +0800

    [fix] Fix wrong arg name.

--- SiteReservation/utils.py
+++ SiteReservation/utils.py
@@ -3,7 +3,7 @@
 # Author: David
 # Email: youchen.du@gmail.com
 # Created: 2017-09-21 18:19
-# Last modified: 2017-09-25 09:31
+# Last modified: 2017-09-25 09:32
 # Filename: utils.py
 # Description:
 from typing import Iterable
@@ -23,6 +23,6 @@ def is_conflict(start, end, site, ignore=None):
         if isinstance(ignore, Iterable):
             q = q.exclude(uid__in=ignore)
         else:
-            q = q.exclude(uid=uid)
+            q = q.exclude(uid=ignore)
     cnt = q.count()
     return cnt != 0
