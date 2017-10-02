8a0ea2a88a2a2a65607cfd6771b1913a79481790
David <youchen.du@gmail.com>
2017-10-02 15:11:17 +0800

    [add] password change.

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,11 +3,12 @@
 # Author: David
 # Email: youchen.du@gmail.com
 # Created: 2017-09-07 09:10
-# Last modified: 2017-09-14 10:49
+# Last modified: 2017-10-02 15:10
 # Filename: urls.py
 # Description:
 from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 from . import views
 from .forms import LoginForm
@@ -38,6 +39,13 @@ urlpatterns = [
         form_class=LoginForm), name='login'),
     url('^logout/', auth_views.LogoutView.as_view(next_page='index'),
         name='logout'),
+    url('^password_change/', auth_views.PasswordChangeView.as_view(
+        template_name='authentication/password_change.html',
+        success_url=reverse_lazy('auth:password_change_done')),
+        name='password_change'),
+    url('^password_change_done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='authentication/password_change_done.html'),
+        name='password_change_done'),
     url('^register/', include(register_patterns, namespace='register')),
     url('^info/', include(info_patterns, namespace='info')),
 ]
