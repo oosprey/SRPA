637053b3d98182515d0411eff65dbafa691561c7
David <youchen.du@gmail.com>
2017-10-02 22:37:18 +0800

    [update] update before starting.

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "Initializing Django server"
 cd SRPA
+echo "Pulling new changes from git repository"
+git pull
+echo "Migrating databases"
 python manage.py migrate
+echo "Starting uwsgi"
 uwsgi --ini web/SRPA_uwsgi.ini
